64817c291e26e5925775ab54f27ffa8dda470d5b
Jonathan Koss <jonathan_koss@yahoo.com>
2019-08-19 19:48:50 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(pigeon_data)
-   
+  output_hash = {}
+  pigeon_data.each do |key, value|
+    value.each do |value_to_push, names_array|
+      names_array.each do |name|  
+        if !output_hash[name]
+          output_hash[name] = {}
+        end 
+        if !output_hash[name][key]
+          output_hash[name][key] = []
+        end 
+        output_hash[name][key] << value_to_push.to_s 
+      end 
+    end 
+  end 
+  output_hash
 end 

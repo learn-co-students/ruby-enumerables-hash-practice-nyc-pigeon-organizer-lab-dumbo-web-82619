372bfbd362d6cc951287ac51b4ca36f3757e0f40
Brett Saxon <brettcsaxon@gmail.com>
2019-08-21 18:40:23 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+def nyc_pigeon_organizer(pigeon_data)
+    pigeon_list = {}
+
+    pigeon_data.each do |property, property_hash|
+     property_hash.each do |values, pigeon_array|
+      pigeon_array.each do |name|
+        if pigeon_list[name]
+          if pigeon_list[name][property]
+            pigeon_list[name][property] << values.to_s
+          else
+            pigeon_list[name][property] = [values.to_s]
+          end
+        else
+          pigeon_list[name] = { property => [values.to_s] }
+        end
+      end
+    end
+  end
+    pigeon_list
+  end

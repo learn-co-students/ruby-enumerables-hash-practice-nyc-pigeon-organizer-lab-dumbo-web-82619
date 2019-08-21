bcfdfa6902dfccb95c4d486d5ea1f1069a43df74
Kayla Petersen <petersen1994@gmail.com>
2019-08-21 21:10:18 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,28 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+  data.reduce({}) do |hash, (key, value)|
+    value.each do |type, names|
+      names.each do |name|
+        
+      if hash[name]
+        if hash[name][key]
+          hash[name][key] << type.to_s
+        else 
+          hash[name][key] = [type.to_s]
+        end
+      else 
+        hash[name] = {key => [type.to_s]}
+      end
+      end
+    end
+    hash
+  end
+end                     
+
+
+#key = "color"
+#value = whole hash of color => names
+#type = type of color 
+#name = name 
+#memo = {}

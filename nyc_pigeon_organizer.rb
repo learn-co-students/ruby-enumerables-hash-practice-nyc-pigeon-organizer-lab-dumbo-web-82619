c8c311bdaffff3432e8743210aba433ce382d75b
Megan Edmonds <meganr.edmonds@gmail.com>
2019-08-21 20:38:37 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,17 +1,16 @@
 require "pry"
 def nyc_pigeon_organizer(data)
   data = data.reduce({}) do |memo, (key, values)|
-    values.each do |item|
-      names = item[1]
+    values.each do |type, names|
       names.each do |name|
         if memo[name]
           if memo[name][key]
-          memo[name][key] << item[0].to_s
+          memo[name][key] << type.to_s
           else
-          memo[name][key] = [item[0].to_s]
+          memo[name][key] = [type.to_s]
           end
         else
-          memo[name] = {key => [item[0].to_s]}
+          memo[name] = {key => [type.to_s]}
         end
       end
     end

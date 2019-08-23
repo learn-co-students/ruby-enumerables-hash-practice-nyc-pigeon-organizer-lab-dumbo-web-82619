6be1aa613b7f0cc8e4ef5be4823b06097c2ae3d6
Tristan Myers <tristifano@gmail.com>
2019-08-23 14:55:43 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  updated_pigeons = {}
+  data.each {|type, info|
+   info.each {|sub_info,names|
+    names.each {|name|
+      name_hash = {}
+      name_hash[type] = [sub_info.to_s]
+      if updated_pigeons[name] && updated_pigeons[name][type]
+        updated_pigeons[name][type].push(sub_info.to_s)
+      elsif updated_pigeons[name]
+        old_name_hash = updated_pigeons[name]
+        new_name_hash = old_name_hash.merge(name_hash)
+        updated_pigeons[name] = new_name_hash
+      else
+      updated_pigeons[name] = name_hash
+      end
+    }
+   }
+  }
+ updated_pigeons
 end
